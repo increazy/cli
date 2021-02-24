a6d51724cc0180108fe1bf2d9ff365e5db2c49e1
Leonardo Vilarinho <leonardo-i@outlook.com>
2021-02-24 12:35:03 -0300

    corrigindo bugs

--- src/commands/task/pull-task.js
+++ src/commands/task/pull-task.js
@@ -5,8 +5,9 @@ module.exports = async(cli, task, fromList) => {
         await cli.git(`pull origin ${task} --force`)
         cli.echo('yellow', '🚸 This task already exists, we recovered it, now it\'s just coding!')
     } else {
+        await cli.git('add .')
         await cli.git(`commit -m "increazy: create task '${task}'"`)
-        await cli.git(`push origin ${task} --force`)
+        await cli.git(`push origin ${task}`)
         cli.echo('green', '✅ We created the task for you, now just code!')
     }
 }
