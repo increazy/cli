2884c6f6aeb0c96e28231af6b7c9232c254fa034
Leonardo Vilarinho <leonardo-i@outlook.com>
2021-02-24 10:00:31 -0300

    muda pasta .data

--- src/commands/login/auth-with-credentials.js
+++ src/commands/login/auth-with-credentials.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 module.exports = async(cli, email, password) => {
     const response = (await cli.http('p:/auth', 'post', { email, password })).data
     const { user } = response
-    fs.mkdirSync(path.resolve(__dirname, '../../.data'), { recursive: true })
+    fs.mkdirSync(path.resolve(__dirname, '../../../.data'), { recursive: true })
     cli.file.write('.auth', user.cli_key)
 
     return user
